ada24ed3d82e561ad2a8ce4040dae9e3b3af592e
Katiuscia <katiusciabalia@gmail.com>
2025-05-20 10:31:59 +0200

    sistemati cors

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 // --- CORS ---
-$allowed_origin = 'http://localhost:5173';
+// Aggiungi il dominio del frontend Netlify
+$allowed_origins = [
+    'https://parentup.netlify.app',
+    'http://localhost:5173' //  sviluppo locale
+];
 
-if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] === $allowed_origin) {
-    header("Access-Control-Allow-Origin: $allowed_origin");
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
     header("Access-Control-Allow-Credentials: true");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
@@ -16,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 }
 
 // --- SESSIONE ---
-// In sviluppo (HTTP): NON usare Secure
+// Produzione: solo se usi HTTPS
 ini_set('session.cookie_samesite', 'None');
 ini_set('session.cookie_secure', '1');
 session_start();
